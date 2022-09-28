36de8326e8a734a7c19b966115119f982f347dfc
iwright5123 <iwright5123@tamu.edu>
2022-09-28 16:21:36 -0500

    CSV File Reading in Java
    This is the code from the lab slides to read csv files using java.

--- test.java
+++ test.java
@@ -1,3 +1,20 @@
-public class test {
+// public class test {
     
-}
+// }
+
+import java.io.*;  
+import java.util.Scanner;  
+public class ReadCSVExample 
+{  
+public static void main(String[] args) throws Exception  
+{  
+//parsing a CSV file into Scanner class constructor  
+Scanner sc = new Scanner(new File("F:\\CSVDemo.csv"));  
+sc.useDelimiter(",");   //sets the delimiter pattern  
+while (sc.hasNext())  //returns a boolean value  
+{  
+System.out.print(sc.next());  //find and returns the next complete token from this scanner  
+}   
+sc.close();  //closes the scanner  
+}  
+}  
