e771e37e2e319293e3aa16fc644daddbcfd0cc52
Kevin Brown <kevinbrown5124@gmail.com>
2022-09-28 15:54:58 -0500

    basic maven project and jdbc poc

---
+++ src/main/java/edu/tamu/spinnstone/App.java
@@ -0,0 +1,17 @@
+package edu.tamu.spinnstone;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        migration.up();
+    }
+}
